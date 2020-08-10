702c04cb27db7a447bdd4f485385f7e0513e69d9
Shakeeb Ahmed <Shakeebazaheer@gmail.com>
2020-08-10 17:05:26 +0000

    List & Dir etc

--- main.py
+++ main.py
@@ -11,3 +11,27 @@ y = "1" #String
 z = 10.1 #float
 
 print(x,y,z) 
+
+print(x + z)
+
+#List data types and examples of range()
+
+listtype = list(range(1,11))
+
+print(listtype)
+
+anotherList = [1,2,3,4,5,6,7,8,9]
+
+print(anotherList)
+
+temperatures = [1 , 2.1 , "1"]
+
+print(temperatures)
+
+rainfall = [1 , 2.1 , "1" , temperatures]
+
+print(rainfall)
+
+#dir() and help() examples
+dir(list)
+help(list.append)
