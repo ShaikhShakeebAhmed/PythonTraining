22862587ce234997f824cb07c59520d4ec099418
Shakeeb Ahmed <Shakeebazaheer@gmail.com>
2020-08-10 05:26:56 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#Importing and printing example
+import datetime
+
+mynow = datetime.datetime.now()
+
+print("The Date time is " ,mynow)
+
+#Declaring Variables no need of type declarations as they are declared implicitly 
+x = 1 #Number
+y = "1" #String
+z = 10.1 #float
+
+print(x,y,z)
