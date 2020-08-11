d9c97b0f83350e56cfa840a84a81ecb8d1c0fd51
Shakeeb Ahmed <Shakeebazaheer@gmail.com>
2020-08-11 19:57:43 +0000

    Dictionaries

--- OperationswithDataTypes.py
+++ OperationswithDataTypes.py
@@ -26,14 +26,22 @@ print(workdays)
 #Slice examples and negative indexing
 
 print(workdays[2:5])
+#>> first 3 
+print(workdays[:3])
 #>> negative indexing -1 to get the last item in the list
 
 print(workdays[-1])
 print(workdays[-4:-1])
 print(workdays[0:-1])
+#>> last 3 
+print(workdays[-3:])
 
 #>> slice of strings
 stringSlice = ["Shakeeb", 1,2,3,4,5]
 
 print(stringSlice[0]) #gets the first value
 print(stringSlice[0][2:-1])
+
+#Dictionary
+phone_numbers = {"John Smith":"+37682929928","Marry Simpsons":"+423998200919"}
+print(phone_numbers["Marry Simpsons"])
