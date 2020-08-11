e71fa4410a3f17d25a2162fbce0d6d98c65b9af8
Shakeeb Ahmed <Shakeebazaheer@gmail.com>
2020-08-11 15:02:47 +0000

    Get and append item

--- OperationswithDataTypes.py
+++ OperationswithDataTypes.py
@@ -15,5 +15,12 @@ print(studentGrade)
 studentGradeget = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
 print(studentGradeget[3])
 
+#>> Get and append item
+
+workdays = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+weekend = ["Sat", "Sun"]
+workdays.append(weekend[0])
+
+print(workdays)
 
 
