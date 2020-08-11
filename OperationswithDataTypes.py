69d3c77581f35df73fb1dec42ad6432e160b6905
Shakeeb Ahmed <Shakeebazaheer@gmail.com>
2020-08-11 19:49:58 +0000

    More Slices and negative indexes

--- OperationswithDataTypes.py
+++ OperationswithDataTypes.py
@@ -26,5 +26,14 @@ print(workdays)
 #Slice examples and negative indexing
 
 print(workdays[2:5])
-print(workdays[-4:-1])
+#>> negative indexing -1 to get the last item in the list
 
+print(workdays[-1])
+print(workdays[-4:-1])
+print(workdays[0:-1])
+
+#>> slice of strings
+stringSlice = ["Shakeeb", 1,2,3,4,5]
+
+print(stringSlice[0]) #gets the first value
+print(stringSlice[0][2:-1])
