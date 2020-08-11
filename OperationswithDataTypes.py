280d9cf573d2db0c030b0cc5ead125ed8ddf694f
Shakeeb Ahmed <Shakeebazaheer@gmail.com>
2020-08-11 12:24:08 +0000

    index  , remove , __getitem__
    more on data structures

---
+++ OperationswithDataTypes.py
@@ -0,0 +1,19 @@
+#Indexing a list
+
+print(help(list.index))
+
+studentGrade = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
+
+#>> search for 10.0 after index 3
+print(studentGrade.index(10.0 , 3))
+
+#>> Remove from the list
+studentGrade.remove(10.0)
+print(studentGrade)
+
+#>> __getitem__ method on a list
+studentGradeget = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
+print(studentGradeget[3])
+
+
+
