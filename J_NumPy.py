fe61903f0476eecb8061ebdea84209e340a965c7
Shakeeb Ahmed <Shakeebazaheer@gmail.com>
2020-08-21 13:14:05 +0000

    Numpy intro

---
+++ J_NumPy.py
@@ -0,0 +1,9 @@
+import numpy
+
+arraytest = [1,2,3,4,5,6,7,8,9,0,12,334,54,76]
+
+npArray = numpy.array(arraytest)
+
+print(arraytest)
+print(npArray)
+print(npArray.reshape(3,3))
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import A_BasicDataStructures
-import B_OperationswithDataTypes
-import C_FunctionsAndConditionals
-import D_ProcessingUserInputs
-import E_BasicLoops
-import F_SmallProgram
-import G_ListComprehensions
-import H_FileProcessing
-import I_Pandas
+#import A_BasicDataStructures
+#import B_OperationswithDataTypes
+#import C_FunctionsAndConditionals
+#import D_ProcessingUserInputs
+#import E_BasicLoops
+#import F_SmallProgram
+#import G_ListComprehensions
+#import H_FileProcessing
+#import I_Pandas
+import J_NumPy
 
 #Important information to know
 #import sys
